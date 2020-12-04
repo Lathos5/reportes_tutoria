c7814ab4b42fda22013ee96f3cb1af61b3eac857
Lathos5 <abrahamehf98@hotmail.com>
2020-12-03 19:11:37 -0600

    RTutorias 1.0.0v - BETA
    Se integra a la ultima versión para la operabilidad completa.
    
    Por agregar -> LOG OUT function | ABOUT template

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from jinja2 import Environment, FileSystemLoader
-from flask import Flask, render_template, request, url_for, redirect, make_response
+from flask import Flask, render_template, request, url_for, redirect, make_response, session
 from flaskext.mysql import MySQL
 import os
 import pdfkit 
@@ -18,12 +18,19 @@ mysql = MySQL(app)
 #ajustes
 app.secret_key = 'mysecretkey'
 
+@app.before_request
+def session_management():
+   session.permanent = True
+
 @app.route('/') #formulario de inicio de sesion >> debe ser la inicial
 def login():
    return render_template('home.html')
 
 @app.route('/log_attempt', methods = ['POST'])
-def logging():
+def loggin():
+
+   session.clear()
+
    flag = False
    if request.method == 'POST':
 
@@ -31,8 +38,9 @@ def logging():
       current_date = request.form['current_date']
       pwd = request.form['pwd']
 
-      print(current_date)
- 
+      session['date'] = current_date
+
+
       conn = mysql.connect()
       pointer = conn.cursor()
 
@@ -44,6 +52,8 @@ def logging():
 
             pointer.execute('SELECT id_tutor FROM maestros WHERE correo = %s', email[0])
             id_Tutor = pointer.fetchone()
+
+            session["auth"] = id_Tutor
 
             pointer.execute('SELECT password FROM maestros WHERE correo = %s', email[0])
             _getpass = pointer.fetchone()
@@ -61,6 +71,9 @@ def logging():
          pointer.execute('SELECT id_tutor, nombre_tutor, apep_tutor, apem_tutor FROM maestros WHERE id_tutor = %s', id_Tutor[0])
          tutores = pointer.fetchall()
 
+         for tutor in tutores:
+            session['user'] = correo        
+
          return render_template('tutor_frame.html', tutelados = alumnos, tutores = tutores, date = current_date)
       else: 
          return 'Error de sesion. Vuelva a ingresar Datos'
@@ -68,7 +81,81 @@ def logging():
    
 @app.route('/main') #muestra de tutelados >> sera la primera vista una ves ingresadas las credeniales
 def home():
-   return render_template('tutor_frame.html')
+   try:
+      user = session['user']
+      auth = session['auth']
+      current_date = session['date']
+   except:
+      user = "Unknown"
+      auth = 0
+      current_date = None
+
+   if auth == 0:
+      return 'Fallo en la sesion'
+   else:
+      conn = mysql.connect()
+      pointer = conn.cursor()
+
+      pointer.execute('SELECT * FROM alumnos WHERE tutor = %s', auth)
+      alumnos = pointer.fetchall()
+
+      pointer.execute('SELECT id_tutor, nombre_tutor, apep_tutor, apem_tutor FROM maestros WHERE id_tutor = %s', auth)
+      tutores = pointer.fetchall()
+      
+      return render_template('tutor_frame.html', tutelados = alumnos, tutores = tutores, date = current_date)
+
+
+@app.route('/new')
+def _new():
+   carreras = ['ITI','LAG','LMKT','ISTI','ITMA','ITEM']
+   semestres = ['PRIMER','SEGUNDO','TERCER','CUARTO','QUINTO','SEXTO','SÉPTIMO','OCTAVO','NOVENO']
+   grupos = ['UPSLP1', 'UPSLP2', 'UPSLP3', 'UPSLP4', 'UPSLP5', 'UPSLP6', 'T-800', 'T-300']
+   return render_template('nuevo_reg.html', carreras = carreras, semestres = semestres, grupos = grupos)
+
+
+@app.route('/insert', methods=['POST'])
+def _insert():
+   if request.method == 'POST':
+
+      tutelado = request.form['nombre']
+      carrera = request.form['carrera_select']
+      semestre = request.form['semestre_select']
+      grupo = request.form['grupo_select']
+      bachillerato = request.form['promedio']
+      reprobadas = str(request.form['reprobadas'])
+      firma = request.form['firma']
+      fecha = request.form['fecha_tutoria']
+      hora = request.form['hora_tutoria']
+
+      try:
+         user = session['user']
+         auth = session['auth']
+         current_date = session['date']
+      except:
+         user = "Unknown"
+         auth = 0
+         current_date = None
+
+      if auth == 0:
+
+         return 'Error. Sesion Fallida'
+      
+      else:
+
+         conn = mysql.connect()
+         pointer = conn.cursor()
+
+         pointer.execute('SELECT * FROM alumnos WHERE tutor = %s', auth)
+         alumnos = pointer.fetchall()
+
+         pointer.execute('SELECT id_tutor, nombre_tutor, apep_tutor, apem_tutor FROM maestros WHERE id_tutor = %s', auth)
+         tutores = pointer.fetchall()
+
+         pointer.execute('INSERT INTO alumnos (fullname_alumno, carrera, semestre, grupo, promedio_bach, materias_reprobadas, firma_estudiante, fecha, horario, tutor) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',( tutelado, carrera, semestre, grupo, bachillerato, reprobadas, firma, fecha, hora, auth))
+         conn.commit()
+  
+
+   return render_template('tutor_frame.html', tutelados = alumnos, tutores = tutores, date = current_date)
 
 
 @app.route('/print/', methods = ['GET'])
@@ -107,7 +194,20 @@ def print_report():
 
 @app.route('/about')
 def about():
-   return render_template('about_frame.html')
+   try:
+      user = session['user']
+      auth = session['auth']
+      current_date = session['date']
+   except:
+      user = "Unknown"
+      auth = 0
+      current_date = None
+
+   if auth == 0:
+      return 'Fallo en la sesion'
+   else:
+      
+      return render_template('about_frame.html')
 
 if __name__ == '__main__':
    app.run(debug=True, port = 3000)
